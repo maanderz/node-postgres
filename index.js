f559cdd4b2d5e8a15564c0199775b5aa1813d8fc
Mandy Arens <mandyfung93@gmail.com>
2020-04-26 14:30:10 -0600

    added bodyparser and nodemon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const bodyParser = require("body-parser");
 const questions_model = require("./questions_model");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json())
 app.use(function (req, res, next) {
@@ -11,14 +14,19 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/', (req, res) => {
-  questions_model.getQuestion()
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
+app.get('/', (req,res) => {
+  res.status(200).send('Initiated')
+})
+
+app.get('/:id', (req, res) => {
+  console.log('id?',req.params.id)
+  // questions_model.getQuestion(id)
+  // .then(response => {
+  //   res.status(200).send(response);
+  // })
+  // .catch(error => {
+  //   res.status(500).send(error);
+  // })
 })
 
 app.listen(port, () => {
